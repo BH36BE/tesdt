1fb161431c72bebf96d4c8cd18ea910310c6dbf6
OzzysCmAcc <oskarkratochvil86@yahoo.com>
2018-04-30 13:17:11 +0200

    mido: add TARGET_RPM_SYSTEM_STAT
    * Rely on rpm stats as we don't have system stats
    
    * Henkate: /d/system_stats is missing for some reason, although it's in the kernel (drivers/soc/qcom/system_stats) and is getting compiled ( obj-$(CONFIG_MSM_RPM_STATS_LOG) += rpm_stats.o rpm_master_stat.o rpm_rail_stats.o system_stats.o ). So tell RPM_SYSTEM_STAT to use /d/rpm_stats which exists. It is told to use /d/system_stats here: https://github.com/LineageOS/android_hardware_qcom_power/blob/lineage-15.1/power-helper.c#L68
    * It's fixing the following error:
    
    E QCOM PowerHAL: extract_stats: failed to open: /d/system_stats Error = No such file or directory
    
    Change-Id: I4aff0958a44f8d5bcd8b6e32c0f0947549fea903

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,6 +185,7 @@ TARGET_PER_MGR_ENABLED := true
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 TARGET_PROVIDES_POWERHAL := true
+TARGET_RPM_SYSTEM_STAT := /d/rpm_system_stats
 
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
