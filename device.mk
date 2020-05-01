25d893df56323dd045eda98ecc207fc2fd727d0a
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-05-01 14:48:59 -0300

    mido: powerstats: Pixel -> Xiaomi
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- device.mk
+++ device.mk
@@ -288,7 +288,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.mido-libperfmgr \
-    android.hardware.power.stats@1.0-service.pixel
+    android.hardware.power.stats@1.0-service.xiaomi
 
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0.vendor \
