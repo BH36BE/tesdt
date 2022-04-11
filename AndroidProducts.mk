d453e572ab47c82c6b0d3575ec3c8ef55f95d6c6
ikeh045 <njkampret@gmail.com>
2022-04-11 16:54:29 +0000

    Nusantara Ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_mido.mk
+    $(LOCAL_DIR)/nad_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_mido-eng \
-    ancient_mido-userdebug \
-    ancient_mido-user
+    nad_mido-eng \
+    nad_mido-userdebug \
+    nad_mido-user
