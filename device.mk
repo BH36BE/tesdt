ea24ddddbed45707d73ac173987e43813c306c65
OdSazib <odsazib@gmail.com>
2022-04-14 15:29:44 +0000

    mido: Build power stats and label it

--- device.mk
+++ device.mk
@@ -290,6 +290,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-service \
     android.hardware.power@1.0-impl \
+    android.hardware.power.stats@1.0-service.mock \
     power.qcom
 
 # Perf
